99ec6fede42b9602d328e744599f7bf8a6d07112
m1ghty07 <dulchenko_s@ukr.net>
2020-12-10 19:49:34 +0200

    main page done

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 $(function(){
-  $('.slider__wrapper').slick({
+  $('.main-slider__wrapper').slick({
     dots:true,
     arrows:false,
   });
 
    var mixer = mixitup('.products__items');
+   
+   var mixer = mixitup('.design__items');
 
 });
