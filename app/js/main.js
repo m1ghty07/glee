90b346181d237086bf28b2108315501ecbbfdfe1
m1ghty07 <dulchenko_s@ukr.net>
2021-02-09 13:38:18 +0200

    na nout

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
 $(function(){
+
+   $('.star').rateYo({
+    starWidth: "10px",
+    ratedFill: "#ffcc00"
+  });
+
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500,
+        postfix: ".00",
+        prefix: "$",
+    });
+
+
   $('.main-slider__wrapper').slick({
     dots:true,
     arrows:false,
@@ -8,4 +25,12 @@ $(function(){
    
    var mixer = mixitup('.design__items');
 
+
 });
+
+
+
+
+
+
+
